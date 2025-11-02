ec110165504d805281dc1a563566677e0051482e
mark btesh <markbtesh@gmail.com>
2025-11-02 14:31:23 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ function App() {
 
 
   {/* Contact Form Section */}
-  <div className="absolute bottom-0 left-0 right-0 min-h-screen bg-black py-32 px-6 overflow-hidden content-end">
+  <div className="absolute bottom-0 left-0 right-0 min-h-screen bg-black py-32 px-6 overflow-hidden content-center">
         {/* Background effects */}
         <div className="absolute inset-0">
           <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-[#3CDCDC]/5 rounded-full blur-3xl animate-[slowRotate_120s_linear_infinite]" style={{ transformOrigin: 'center center' }}></div>
